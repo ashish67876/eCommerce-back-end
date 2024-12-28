ad52515b550da1a08db8f42eff0f85577c9bb9f2
root <root@instance-20241225-043322.asia-south2-c.c.gke-cluster-442710.internal>
2024-12-28 14:18:26 +0000

    isolated

--- auth-service/src/main/java/com/example/authservice/dtos/UserHeader.java
+++
@@ -1,7 +0,0 @@
-package com.example.authservice.dtos;
-
-public record UserHeader(
-        Long id,
-        String username
-) {
-}
--- order-service/src/main/java/com/example/orderservice/repository/IOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.orderservice.repository;
-
-import com.example.orderservice.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOrderRepository
-        extends JpaRepository<Order, Long> {
-}
